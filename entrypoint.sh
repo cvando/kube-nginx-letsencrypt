4af97232cbbd971854e56b52f1b7e5e4cede2654
clement.vandoolaeghe.e <clement.vandoolaeghe.e@thalesdigital.io>
2020-04-15 17:36:46 +0200

    Storageos etcd secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,62 +1,26 @@
 #!/bin/bash
 
-if [[ -z $EMAIL || -z $DOMAINS || -z $SECRET ]]; then
-	echo "EMAIL, DOMAINS, SECRET env vars required"
-	env
-	exit 1
-fi
-
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-
-cd $HOME
-
-echo "Listen python"
-python -m SimpleHTTPServer 80 &
-sleep 60
-
-echo "End sleep starting certbot"
-PID=$!
-
-if [ $STAGING ]; then
-	if [ $STAGING = true ]; then
-		certbot certonly --webroot -w $HOME -n --agree-tos --email ${EMAIL} --no-self-upgrade -d ${DOMAINS} --staging
-	else
-		certbot certonly --webroot -w $HOME -n --agree-tos --email ${EMAIL} --no-self-upgrade -d ${DOMAINS}
-	fi
-else
-	certbot certonly --webroot -w $HOME -n --agree-tos --email ${EMAIL} --no-self-upgrade -d ${DOMAINS}
-fi
-
-kill $PID
-
-CERTPATH=/etc/letsencrypt/live/$(echo $DOMAINS | cut -f1 -d',')
-
-ls $CERTPATH || exit 1
+CA=$(cat /etc/kubernetes/ssl/kube-ca.pem | base64)
+CERT=$(cat /etc/kubernetes/ssl/kube-etcd-192.168.*.*.pem | base64)
+KEY=$(cat /etc/kubernetes/ssl/kube-etcd-192.168.*.*-key.pem | base64)
 
 cat /secret-patch-template.json | \
-	sed "s/NAMESPACE/${NAMESPACE}/" | \
-	sed "s/NAME/${SECRET}/" | \
-	sed "s/TLSCERT/$(cat ${CERTPATH}/fullchain.pem | base64 | tr -d '\n')/" | \
-	sed "s/TLSKEY/$(cat ${CERTPATH}/privkey.pem |  base64 | tr -d '\n')/" \
+	sed "s/CA/${CA}/" | \
+	sed "s/CERT/${CERT}/" | \
+	sed "s/KEY/$(KEY)/" \
 	> /secret-patch.json
 
 ls /secret-patch.json || exit 1
 
-echo  "update secret"
-RESP=`curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/secret-patch.json https://kubernetes.default/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}`
-CODE=`echo $RESP | jq -r '.code'`
+echo "Create secret ${SECRET}"
+RESP=`curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPOST  -H "Accept: application/json, */*" -H "Content-Type: application/json" -d @/secret-patch.json https://kubernetes.default/api/v1/namespaces/storageos/secrets`
+echo $RESPCODE=`echo $RESP | jq -r '.code'`
 
 case $CODE in
 200)
-	echo "Secret Updated"
+	echo "Secret Created"
 	exit 0
-	;;
-404)
-	echo "Secret doesn't exist"
-	echo "Create secret ${SECRET}"
-	RESP=`curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPOST  -H "Accept: application/json, */*" -H "Content-Type: application/json" -d @/secret-patch.json https://kubernetes.default/api/v1/namespaces/${NAMESPACE}/secrets`
-	echo $RESP
-	# echo "Create secret ${SECRET}"
 	;;
 *)
 	echo "Unknown Error:"
