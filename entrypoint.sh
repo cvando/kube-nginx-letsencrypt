6feebe1247ee2c236df60d56ce559dbd8fe7ae72
clement.vandoolaeghe.e <clement.vandoolaeghe.e@thalesdigital.io>
2020-04-16 15:00:11 +0200

    Storageos etcd secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ CA=$(cat /etc/kubernetes/ssl/kube-ca.pem | base64)
 CERT=$(cat /etc/kubernetes/ssl/kube-etcd-192-168-[0-9]*-[0-9]*.pem | base64)
 KEY=$(cat /etc/kubernetes/ssl/kube-etcd-192-168-[0-9]*-[0-9]*-key.pem | base64)
 
-sed -i "s@CA@${CA}@" /secret-patch.json 
-sed -i "s|CERT|${CERT}|" /secret-patch.json
-sed -i "s|KEY|${KEY}|" /secret-patch.json
+sed -i 's|CA|${CA}|g' /secret-patch.json 
+sed -i 's|CERT|${CERT}|g' /secret-patch.json
+sed -i 's|KEY|${KEY}|g' /secret-patch.json
 
 
 echo "Create secret"
