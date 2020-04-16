cdb70732e98c3b61bfe49adde50524576189e3ad
clement.vandoolaeghe.e <clement.vandoolaeghe.e@thalesdigital.io>
2020-04-16 15:26:53 +0200

    Storageos etcd secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,9 @@ CA=$(cat /etc/kubernetes/ssl/kube-ca.pem | base64)
 CERT=$(cat /etc/kubernetes/ssl/kube-etcd-192-168-[0-9]*-[0-9]*.pem | base64)
 KEY=$(cat /etc/kubernetes/ssl/kube-etcd-192-168-[0-9]*-[0-9]*-key.pem | base64)
 
-sed -i "s|CA|${CA}|g" /secret-patch.json 
-sed -i "s|CERT|${CERT}|g" /secret-patch.json
-sed -i "s|KEY|${KEY}|g" /secret-patch.json
-
+sed -i 's|CA|'$CA'|g' /secret-patch.json 
+sed -i 's|CERT|'$CERT'|g' /secret-patch.json
+sed -i 's|KEY|'$KEY'|g' /secret-patch.json
 
 echo "Create secret"
 RESP=`curl --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPOST  -H "Accept: application/json, */*" -H "Content-Type: application/json" -d @/secret-patch.json https://kubernetes.default/api/v1/namespaces/storageos/secrets`
